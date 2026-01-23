0ff2b4d8d3d0d6c45a48ae52a9661bb655be400e
almukhammed77 <amantaialmukhammed@gmail.com>
2026-01-23 13:22:31 +0500

    "Updated main.go with my commit message"

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Handbook of Programming Languages — Milestone 3 skeleton")
 	fmt.Println("This is just planning stage. Full code later.")
+	fmt.Println("My commit from Almukhammed")
 }
