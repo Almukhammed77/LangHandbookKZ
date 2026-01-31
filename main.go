8fa52b1170f139f0b0cfef3f9a763dceb3c1209c
almukhammed77 <amantaialmukhammed@gmail.com>
2026-01-31 20:44:26 +0500

    "implement HTTP server with 3 endpoints: GET /api/languages, GET /api/languages/{id}, POST /api/languages with JSON body"

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
 	http.HandleFunc("/api/languages", handlers.LanguagesHandler)
 	http.HandleFunc("/api/languages/", handlers.GetLanguageByID)
 
-	log.Println("Сервер запущен на http://localhost:8080")
+	log.Println("Server conn on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
